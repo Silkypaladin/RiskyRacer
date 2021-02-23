e8d58c2f0ca5f049968e34e80008313c2294d8ae
Silkypaladin <bednarski.szymon99@gmail.com>
2021-02-23 14:39:58 +0100

    Add icons and graphics

---
+++ app/src/main/java/com/example/riskyracer/Constants.java
@@ -0,0 +1,16 @@
+package com.example.riskyracer;
+
+import android.content.Context;
+
+public class Constants {
+
+    public static int SCREEN_HEIGHT;
+    public static int SCREEN_WIDTH;
+    public static Context CURRENT_CONTEXT;
+
+    public static long INIT_TIME;
+    public static float TILT_SPEED_SCALER = 1200;
+    public static int PLAYER_GAP = 250;
+    public static int OBSTACLE_GAP = 500;
+    public static int OBSTACLE_HEIGHT = 50;
+}
